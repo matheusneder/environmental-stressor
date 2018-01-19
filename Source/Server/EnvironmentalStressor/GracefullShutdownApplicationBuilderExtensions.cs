38bf3a8e0c66bfc6162c2576edd2d98876fd809b
Matheus Neder <matheusneder@gmail.com>
2018-01-19 16:00:22 -0200

    Added GrafullShutdown feature

---
+++ Source/Server/EnvironmentalStressor/GracefullShutdownApplicationBuilderExtensions.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace EnvironmentalStressor
+{
+    public static class GracefullShutdownApplicationBuilderExtensions
+    {
+        private static int requestCount = 0;
+        private static object lockPad = new object();
+        private static ILogger logger;
+
+        public static IApplicationBuilder UseGracefullShutdown(this IApplicationBuilder applicationBuilder,
+            IApplicationLifetime applicationLifetime, ILoggerFactory loggerFactory)
+        {
+            logger = loggerFactory.CreateLogger("GracefullShutdown");
+
+            applicationBuilder.Use(async (HttpContext context, Func<Task> next) =>
+            {
+                lock (lockPad)
+                {
+                    requestCount++;
+                }
+
+                await next.Invoke();
+
+                lock (lockPad)
+                {
+                    requestCount--;
+                }
+            });
+
+            applicationLifetime.ApplicationStopping.Register(async () =>
+            {
+                logger.LogInformation("Application stopping, waiting for pending requests to complete...");
+
+                do
+                {
+                    await Task.Delay(1000);
+                    logger.LogInformation($"Current request count: {requestCount}");
+                }
+                while (requestCount > 0);
+
+                logger.LogInformation("Done! Application will now stop.");
+            });
+
+            return applicationBuilder;
+        }
+    }
+}
--- Source/Server/EnvironmentalStressor/Startup.cs
+++ Source/Server/EnvironmentalStressor/Startup.cs
@@ -40,9 +40,10 @@ namespace EnvironmentalStressor
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IApplicationLifetime applicationLifetime)
         {
             ConfigureLog(app, env, loggerFactory);
+            app.UseGracefullShutdown(applicationLifetime, loggerFactory);
             app.UseMvc();
             app.UseSwagger();
             app.UseSwaggerUI(c =>
